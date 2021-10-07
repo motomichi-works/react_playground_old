fc7a470960163f9672e91ab9feed525e1f2ac813
motomichi <motomichi.motomichi@gmail.com>
2021-10-07 18:46:18 +0900

    renamed src/containers/RiakutoChapter09/organisms/Section02.tsx -> src/containers/RiakutoChapter09/organisms/Section02UseState.tsx

--- src/containers/RiakutoChapter09/index.tsx
+++ src/containers/RiakutoChapter09/index.tsx
@@ -1,9 +1,9 @@
-import Section02 from 'containers/RiakutoChapter09/organisms/Section02';
+import Section02UseState from 'containers/RiakutoChapter09/organisms/Section02UseState';
 import Template001 from 'components/common/templates/Template001';
 
 const RiakutoChapter08: React.VFC = () => (
   <Template001 pageHeadingText="RiakutoChapter09">
-    <Section02 />
+    <Section02UseState />
   </Template001>
 );
 
