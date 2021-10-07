3a2fe45fcd9d20ea1d348f7ac11de53fbe802dc7
motomichi <motomichi.motomichi@gmail.com>
2021-10-07 15:52:46 +0900

    8-2 完了

--- src/components/riakuto-chapter08/molecules-and-atoms/CharacterList.tsx
+++ src/components/riakuto-chapter08/molecules-and-atoms/CharacterList.tsx
@@ -1,7 +1,37 @@
-const CharacterList: React.VFC = () => (
-  <div>
-    <p>キャラクターリスト</p>
-  </div>
+import styled from 'styled-components';
+import Heading001 from 'components/common/molecules-and-atoms/Heading001';
+
+export type Character = {
+  id: number;
+  name: string;
+  grade: number;
+  height?: number;
+};
+
+type Props = {
+  school: string;
+  characters: Character[];
+};
+
+const StyledElement = styled.div`
+  .Heading {
+    font-weight: bold;
+  }
+  .List {
+    margin-top: 1em;
+  }
+`;
+const CharacterList: React.VFC<Props> = ({ school, characters }) => (
+  <StyledElement>
+    <Heading001 text={school} modifierClassNames={['Lv3']} />
+    {characters.map((character) => (
+      <ul key={character.id} className="List">
+        <li>{character.name}</li>
+        <li>{character.grade}年生</li>
+        <li>{character.height ? character.height : '???'}cm</li>
+      </ul>
+    ))}
+  </StyledElement>
 );
 
 export default CharacterList;
--- src/containers/RiakutoChapter08/organisms/Section02Props.tsx
+++ src/containers/RiakutoChapter08/organisms/Section02Props.tsx
@@ -1,10 +1,46 @@
 import BasicOrganism001 from 'components/common/organisms/BasicOrganism001';
-import CharacterList from 'components/riakuto-chapter08/molecules-and-atoms/CharacterList';
+import CharacterList, {
+  Character,
+} from 'components/riakuto-chapter08/molecules-and-atoms/CharacterList';
 
-const Section02Props: React.VFC = () => (
-  <BasicOrganism001 sectionHeadingText="Section02 : コンポーネントとProps">
-    <CharacterList />
-  </BasicOrganism001>
-);
+const Section02Props: React.VFC = () => {
+  const characters: Character[] = [
+    {
+      id: 1,
+      name: '桜木花道',
+      grade: 1,
+      height: 189.2,
+    },
+    {
+      id: 2,
+      name: '流川 楓',
+      grade: 1,
+      height: 187,
+    },
+    {
+      id: 3,
+      name: '宮城リョータ',
+      grade: 2,
+      height: 168,
+    },
+    {
+      id: 4,
+      name: '三井 寿',
+      grade: 3,
+    },
+    {
+      id: 5,
+      name: '赤木剛憲',
+      grade: 3,
+      height: 197,
+    },
+  ];
+
+  return (
+    <BasicOrganism001 sectionHeadingText="8-2. コンポーネントとProps">
+      <CharacterList school="湘北高校" characters={characters} />
+    </BasicOrganism001>
+  );
+};
 
 export default Section02Props;
